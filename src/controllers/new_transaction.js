9dc907445b474d192314b26157a6f65bda2f2275
12121895241 <adrian.alexis.vega@gmail,com>
2024-03-31 16:21:10 -0600

    transaction

--- src/controllers/new_transaction.js
+++ src/controllers/new_transaction.js
@@ -13,34 +13,36 @@ const nweTransactions = async (req, res) => {
     //decodificamos la info del token
     const decodedToken = jwt.decode(token); //decodificamos el jwt
     const idUserAccount = decodedToken.userId;
-    
+    console.log(idUserAccount);
+    // realisamos busqueda del ususario y su cuenta  asociada a este id de usuario
     const userWithAccount = await User.findAll({
-      where: { id: idUserAccount }, // buscamos la cuenta del logeado por id de token
+      where: { id: decodedToken.userId }, // buscamos la cuenta del logeado por id de token
       include: [{ model: Account }], // Esto incluirá la cuenta relacionada
     });
+
     //guardamos la informacion en una  variable para poder utilizarla
     const user = userWithAccount[0].toJSON();
-  
     // Verificamos si el usuario tiene una cuenta asociada
     if (!user.Accounts) {
       return res.status(400).json({ error: "User has no associated account" });
     }
-    //buscamos  el numero de cuenta 
+    //buscamos  el numero de cuenta
     const destinationAccount = await Account.findAll({
       where: { account_number: accound_number },
+      include: [{ model: User }], //para saber que usuario es dueño de esta cuenta
     });
-    console.log(destinationAccount)
+
     //verificamos si la cuenta destino existe  y que sea distinta a la de origen
     if (!destinationAccount) {
       return res.status(404).json({ error: "Destination account not found" });
     }
     // verificamos si la cuenta tienen fondos suficientes
-    if (user.Accounts[0].balance.length === 0) {
+    if (user.Accounts.balance <= 0) {
       return res
         .status(500)
         .json({ error: "Insufficient funds in the origin account" });
     }
-    // Verificamos si hay fondos suficientes en la cuenta de origen
+    // Verificamos si hay fondos suficientes en la cuenta de origenmpara la transacion
     if (user.Accounts[0].balance < amount) {
       return res
         .status(400)
@@ -50,15 +52,14 @@ const nweTransactions = async (req, res) => {
     // Realizar la transferencia dentro de una transacción
     await sequelize.transaction(async (transaction) => {
       // Actualizar el saldo de las cuentas de origen y destino
-      user.Accounts[0].balance -= amount;
-      destinationAccount[0].balance += amount;
-      // Actualizar los modelos de cuenta en la base de datos
       await Account.update(
-        { balance: user.Accounts[0].balance },
+        { balance: sequelize.literal(`balance - ${amount}`) },
         { where: { id: user.Accounts[0].id }, transaction }
       );
+
+      // Actualizar el saldo de la cuenta de destino
       await Account.update(
-        { balance: destinationAccount[0].balance },
+        { balance: sequelize.literal(`balance + ${amount}`) },
         { where: { id: destinationAccount[0].id }, transaction }
       );
 
@@ -68,15 +69,13 @@ const nweTransactions = async (req, res) => {
           id_origin_account: user.Accounts[0].id,
           id_destination_account: destinationAccount[0].id,
           amount: amount,
-          id_user : idUserAccount,
+          id_user: idUserAccount,
         },
         { transaction }
       );
     });
 
-    return res
-      .status(200)
-      .json({ message: "Transaction completed successfully" });
+    return res.status(200).json("Transaction completed successfully");
   } catch (error) {
     console.error("Error:", error);
     return res.status(500).json({ error: "Internal Server Error" });
